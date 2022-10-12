047a9291e6fa9888d3958102aafd51fe2279f217
Federico Carreño <f.n.carreno@gmail.com>
2022-10-11 23:26:59 -0300

    agrego ejemplos de clase 18

--- ejemplo/views.py
+++ ejemplo/views.py
@@ -2,4 +2,11 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, "ejemplo/saludar.html")
+    return render(request, "ejemplo/saludar.html",{"nombre": "Federico", "apellido": "Carreño"})#paso un diccionario
+
+def index_dos(request, nombre, apellido):
+    return render(request, "ejemplo/saludar.html",{"nombre": nombre, "apellido": apellido})#paso un diccionario
+
+def index_tres(request):
+    return render(request, "ejemplo/saludar.html",
+    { "notas":[1,2,3,4,5,6,7]})
