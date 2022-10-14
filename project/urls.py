2afada413e440479becc0df66db46fbcd86c03ca
Federico Carreño <f.n.carreno@gmail.com>
2022-10-13 23:33:17 -0300

    se agrega la app blog y un template avanzado

--- project/urls.py
+++ project/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
 from ejemplo.views import (imc, index, index_dos, index_tres, imc, monstrar_familiares)
+from blog.views import index as blog_index
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('saludar/', index),
@@ -24,5 +25,7 @@ urlpatterns = [
     path('mostrar-notas/', index_tres),
     path('mostrar-imc/<int:peso>/<int:altura>/', imc),
     path('mi-familia/', monstrar_familiares),
+    path('blog/', blog_index),
+
 ]
 
